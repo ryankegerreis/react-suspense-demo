48d64cec55a5e5361df926d4d45bb9bf72d9da8b
Ryan Kegerreis <Rkegerreis@assemblysoftware.com>
2019-11-15 09:33:05 -0500

    Suspenseful dog

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Image extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      loaded: false,
-      dog: ''
-    }
-  }
+		this.state = {
+			loaded: false,
+			dog: ''
+		};
+	}
 
-  componentDidMount() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(res => res.json())
-      .then(res => console.log(res.message))
-      .then(res => this.setState({ loaded: true, dog: res.message }))
-  }
-
-  render() {
-    return (
-      <div>
-        {console.log(this.state.dog)}
-        <img src={this.state.dog === undefined || '' ? 'https://www.bing.com/th?id=OIP.MX3BTxrVCObd1RhM8oY-aAHaLJ&pid=Api&rs=1' : this.state.dog} className="App-logo" alt="logo" />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				{console.log(this.state.dog)}
+				<img
+					width='400px'
+					src='http://www.hdnicewallpapers.com/Walls/Big/Dog/Beautiful_Dog_Puppy_4K_Wallpaper.jpg'
+				/>
+			</div>
+		);
+	}
 }
